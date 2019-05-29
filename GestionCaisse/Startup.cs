51480a931f2ecb398c25d0e472eea27fddf8d8a3
kawther BJAOUI <kawther.bjaoui@gmail.com>
2019-05-29 13:57:45 +0100

    third commit

--- GestionCaisse/Startup.cs
+++ GestionCaisse/Startup.cs
@@ -52,21 +52,21 @@ namespace GestionCaisse
             });
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-  .AddJwtBearer(options =>
-  {
-      options.TokenValidationParameters = new TokenValidationParameters
-      {
-          ValidateIssuer = true,
-          ValidateAudience = true,
-          ValidateLifetime = true,
-          ValidateIssuerSigningKey = true,
-          ValidIssuer = Configuration["Jwt:Issuer"],
-          ValidAudience = Configuration["Jwt:Issuer"],
-          IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
-      };
-  });
+                    .AddJwtBearer(options =>
+                    {
+                         options.TokenValidationParameters = new TokenValidationParameters
+                         {
+                            ValidateIssuer = true,
+                            ValidateAudience = true,
+                            ValidateLifetime = true,
+                            ValidateIssuerSigningKey = true,
+                            ValidIssuer = Configuration["Jwt:Issuer"],
+                            ValidAudience = Configuration["Jwt:Issuer"],
+                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                         };
+                     });
 
-        }
+                    }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
